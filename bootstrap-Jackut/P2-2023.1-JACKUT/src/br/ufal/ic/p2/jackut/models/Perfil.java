dcfb72c0d32469a4ae77119b57dac3ba1c34fcd2
Carla Evellyn Ferreira dos Santos <129027590+Carlaevellyn@users.noreply.github.com>
2025-03-30 04:57:39 -0300

    Update Perfil.java

--- bootstrap-Jackut/P2-2023.1-JACKUT/src/br/ufal/ic/p2/jackut/models/Perfil.java
+++ bootstrap-Jackut/P2-2023.1-JACKUT/src/br/ufal/ic/p2/jackut/models/Perfil.java
@@ -1,14 +1,19 @@
+// Classe Perfil.java - Representa o perfil de um usuário, armazenando atributos personalizados
 package br.ufal.ic.p2.jackut.models;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class Perfil implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private final Map<String, String> atributos = new HashMap<>();
 
+    // Adiciona um atributo ao perfil do usuário
     public void adicionarAtributo(String chave, String valor) {
         if (chave == null || chave.isEmpty()) {
             throw new RuntimeException("Atributo não preenchido.");
@@ -16,6 +21,7 @@ public class Perfil implements Serializable {
         atributos.put(chave, valor);
     }
 
+    // Retorna o valor de um atributo específico
     public String getAtributo(String chave) {
         return atributos.get(chave);
     }
